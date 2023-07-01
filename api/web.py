e5c3fd9a6790c02ae887d6339470bd626a57b817
yang <zy892065502@gmail.com>
2023-07-01 09:42:45 +0800

    test

--- api/web.py
+++ api/web.py
@@ -1,4 +1,7 @@
 from flask import Flask, Response, request
+import os
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
@@ -8,6 +11,11 @@ def catch_all(path):
     key = request.args.get('key')
     return Response("<h1>Flask</h1><p>You visited: /%s</p><p>key=%s</p>" % (path,key), mimetype="text/html")
 
+@app.route("/api/chart", methods=["GET"])
+def chart():
+        print(request.args)
+        key = request.args.get('key')
+        return Response("<h1>Flask</h1><p>You key: /%s</p><p>key=%s</p>" % (openai_api_key), mimetype="text/html")
     
 if __name__ == "__main__":
     app.run(debug = True)
