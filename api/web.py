9a7d13a09d5f2ac559dc1fd0e6d0bd3292211983
yang <zy892065502@gmail.com>
2023-07-03 16:17:24 +0800

    add prompt

--- api/web.py
+++ api/web.py
@@ -4,26 +4,36 @@ import openai
 from flask_cors import CORS
 from urllib.parse import unquote
 import json
+import prompt
 
 openai_api_key = os.getenv('OPENAI_API_KEY')
 openai.api_key = openai_api_key
 app = Flask(__name__)
 CORS(app)  # 添加这一行以允许跨域请求
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    print(request.args)
-    key = request.args.get('key')
-    return Response("<h1>Flask</h1><p>You visited: /%s</p><p>key=%s</p>" % (path,key), mimetype="text/html")
+@app.route("/api/backend", methods=["GET"])
+def backend():
+    try:
+        content = request.args.get("messages", "")
+        content = unquote(content)  # 解码URL参数
+        content = json.loads(content)
+        # content = content + "。只输出json"
+        print(content)
 
-@app.route("/api/chart", methods=["GET"])
-def chart():
-        print(request.args)
-        key = request.args.get('key')
-        return Response("<h1>Flask</h1><p>You key: </p><p>key=%s</p>" % (openai_api_key), mimetype="text/html")
-    
-
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": prompt.system_message},
+                {"role": "user", "content": content}
+            ],
+            max_tokens=1000,
+            n=1,
+            temperature=0.1,
+        )
+        message = response['choices'][0]['message']['content']
+        return jsonify(message=message)
+    except Exception as err:
+        return jsonify(error=str(err)), 404
 
 @app.route("/api/chatmesg", methods=["GET"])
 def chatmesg():
